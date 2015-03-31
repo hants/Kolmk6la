6e3b16c0ff749817e14dabcd0c6fd5c7e8252b3c
hannor <hannor@greeny.cs.tlu.ee>
2015-03-31 11:23:03 +0300

    fas

---
+++ Kolmk6la/Kolmk6la.java
@@ -0,0 +1,12 @@
+public class Kolmk6la{
+	private int p6hitoon;
+	//private int terts;
+	//private int kvint;
+	public Kolmk6la(int p6hitoon){
+		this.p6hitoon=p6hitoon;
+	}
+	int terts(){return p6hitoon+4;};
+	int kvint(){return p6hitoon+7;};
+	
+}
+
